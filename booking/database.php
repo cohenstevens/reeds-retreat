a889c1f2f9f6e6fc66d37d2b79eb1aaaa00bf702
cohenstevens <csteve54@kent.edu>
2024-08-21 15:54:50 -0400

    update

--- booking/database.php
+++ booking/database.php
@@ -1,5 +1,5 @@
 <?php
-$servername = "localhost"; // Change this if your MySQL server is hosted elsewhere
+$servername = "https://cohenstevens.github.io/reeds-retreat/"; // Change this if your MySQL server is hosted elsewhere
 $dbname = "gmeros"; // Your MySQL database name
 $username = "gmeros"; 
 $password = "5pVKqzx2";
